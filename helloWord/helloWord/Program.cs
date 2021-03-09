1cffc8a474a6b7130bbede5c3d86e2d95b99f306
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-03-09 18:51:48 +0530

    a

--- helloWord/helloWord/Program.cs
+++ helloWord/helloWord/Program.cs
@@ -12,29 +12,24 @@ namespace helloWord
         {
             
 
-            string studentName;
-            int studentAge;
-            float marks;
-            //char section = 'a';
-           // bool stream = trun;
-            Console.Write("your name please:");
-            studentName = Console.ReadLine();
-            Console.WriteLine("-----");
-            Console.Write("Your age please:");
-            studentAge = int.Parse(Console.ReadLine());
-            Console.WriteLine("-----");
-            Console.Write("Your marks please:");
-            marks = float.Parse(Console.ReadLine());
-            Console.WriteLine("-----");
-            Console.WriteLine("type one ");
-            Console.WriteLine("name--- (" + studentName + ")  age---(" + studentAge + ") mark---("+ marks);
-            Console.WriteLine("type two ");
-            Console.WriteLine("Name---("+ studentName);
-            Console.WriteLine("age----("+ studentAge);
-            Console.WriteLine("marks---(" + marks);
-            Console.WriteLine("type three ");
-            Console.WriteLine("name-- {0}  //age-- {1} //mark-- {2}" , studentName, studentAge, marks);
-            Console.Read();
+          
+            int age;
+           
+            
+            Console.Write("plese inter your age :");
+            age = int.Parse(Console.ReadLine());
+            if (age >= 18)
+            {
+                Console.Write("you can vot");
+            }
+            else
+            {
+                Console.Write("you connot vot");
+            }
+            Console.WriteLine();
+            Console.Write("press any key to exit");
+            Console.ReadKey();
+       
         }
     }
 }
